23fe4eb4d1792eb2dc61ee43b65b461cc112a3b3
Julien Richard-Foy <julien@richard-foy.fr>
2012-10-22 20:55:30 +0200

    More help for IntList

--- src/main/scala/tp/IntList.scala
+++ src/main/scala/tp/IntList.scala
@@ -1,38 +1,94 @@
 package tp
 
+/**
+ * IntList public interface
+ */
 sealed abstract class IntList {
 
-  // override def toString: String
+  def foreach(f: Int => Unit): Unit
 
-  // def foreach(f: Int => Unit): Unit
+  def map(f: Int => Int): IntList
 
-  // def map(f: Int => Int): IntList
+  def filter(p: Int => Boolean): IntList
 
-  // def filter(p: Int => Boolean): IntList
+  def sum: Int
 
-  // def fold(z: Int, op: (Int, Int) => Int): Int
+  def product: Int
 
-  // def sum: Int
+  def fold(z: Int, op: (Int, Int) => Int): Int
 
-  // def product: Int
+  def forall(p: Int => Boolean): Boolean
 
-  // def forall(p: Int => Boolean): Boolean
-
-  // def foldBool(z: Boolean, op: (Int, Boolean) => Boolean): Boolean
+  def foldBool(z: Boolean, op: (Int, Boolean) => Boolean): Boolean
 
 }
 
+/**
+ * Empty list
+ */
+class Nil extends IntList {
+
+  override def toString = ???
+
+  def foreach(f: Int => Unit) {
+    ???
+  }
+
+  def map(f: Int => Int): IntList = ???
+
+  def filter(p: (Int) => Boolean) = ???
+
+  def sum = ???
+
+  def product = ???
+
+  def fold(z: Int, op: (Int, Int) => Int): Int = ???
+
+  def forall(p: Int => Boolean): Boolean = ???
+
+  def foldBool(z: Boolean, op: (Int, Boolean) => Boolean): Boolean = ???
+}
+
+/**
+ * List with a `head` element and a `tail` list
+ * @param head Head element
+ * @param tail Tail list
+ */
+class Cons(head: Int, tail: IntList) extends IntList {
+
+  override def toString = ???
+
+  def foreach(f: Int => Unit) {
+    ???
+  }
+
+  def map(f: Int => Int): IntList = ???
+
+  def filter(p: (Int) => Boolean) = ???
+
+  def sum = ???
+
+  def product = ???
+
+  def fold(z: Int, op: (Int, Int) => Int) = ???
+
+  def forall(p: (Int) => Boolean) = ???
+
+  def foldBool(z: Boolean, op: (Int, Boolean) => Boolean) = ???
+}
+
+/**
+ * Convenient factories
+ */
 object IntList {
 
-  def nil = new IntList {
-    // TODO Write some code here
-  }
+  /** @return an empty list */
+  def nil: IntList = new Nil
 
-  def cons(head: Int, tail: IntList) = new IntList {
-    // TODO Write some code here
-  }
+  /** @return a list with a `head` element and a `tail` list */
+  def cons(head: Int, tail: IntList): IntList = new Cons(head, tail)
 
-  // Already implemented. Use it as follows: `IntList(1, 2, 3)`
+  /** @example IntList(1, 2, 3) */
   def apply(xs: Int*): IntList = xs.foldRight(nil)(cons)
 
 }
